f6e476d95235f292cd970e9806942b9875684c06
Vicente Vendramin <guesser.vicente@gmail.com>
2022-04-12 20:54:19 -0300

    partially ready mobile app

---
+++ main.js
@@ -0,0 +1,30 @@
+/* OPEN & CLOSE MENU -- ICONS */
+const nav = document.querySelector('#header nav');
+const toggle = document.querySelectorAll('nav .toggle');
+
+for(const element of toggle) {
+    element.addEventListener('click', function() {
+        nav.classList.toggle('show');
+    })
+}
+
+/* HIDE THE MENU WHEN CLICKIN ON A MENU ICON */
+const links = document.querySelectorAll('nav ul li a');
+
+for(const link of links) {
+    link.addEventListener('click', function() {
+        nav.classList.remove('show');
+    })
+}
+
+/* CHANGE PAGE HEADER WHEN SCROLLING */
+const header = document.querySelector('#header');
+const navHeight = header.offsetHeight;
+
+window.addEventListener('scroll', function() {
+    if(window.scrollY >= navHeight){
+        header.classList.add('scroll');
+    } else {
+        header.classList.remove('scroll');
+    }
+})
