201ea4313ffb91c038067a2a72b6ce126e8a620b
Vicente Vendramin <guesser.vicente@gmail.com>
2022-04-14 11:23:48 -0300

    new footer

--- main.js
+++ main.js
@@ -18,16 +18,16 @@ for(const link of links) {
 }
 
 /* CHANGE PAGE HEADER WHEN SCROLLING */
-const header = document.querySelector('#header');
-const navHeight = header.offsetHeight;
+function changeHeaderWhenScroll() {
+    const header = document.querySelector('#header');
+    const navHeight = header.offsetHeight;
 
-window.addEventListener('scroll', function() {
-    if(window.scrollY >= navHeight){
+    if(window.scrollY >= navHeight) {
         header.classList.add('scroll');
     } else {
         header.classList.remove('scroll');
     }
-})
+}
 
 /* TESTIMONIALS CAROUSEL / SLIDER / SWIPER */
 const swiper = new Swiper('.swiper', {
@@ -52,5 +52,24 @@ scrollReveal.reveal(
     #about .image, #about .text,
     #services header, #services .card,
     #testimonials header, #testimonials .testimonials,
-    #contact .text, #contact .links
+    #contact .text, #contact .links,
+    footer .brand, footer .social
     `, { interval: 100 })
+
+/* BUTTON: Back to top */
+
+function backToTop() {
+    const backToTopButton = document.querySelector('.back-to-top');
+
+    if(window.scrollY >= 560){
+        backToTopButton.classList.add('show');
+    } else {
+        backToTopButton.classList.remove('show');
+    }
+}
+
+/* WHEN SCROLL */
+window.addEventListener('scroll', function() {
+    changeHeaderWhenScroll()
+    backToTop()
+})
